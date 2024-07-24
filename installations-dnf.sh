5736ba12095b55442ecf578d7e6ae4d278d44aa9
xairaven <alexkovalevkpi@gmail.com>
2024-07-24 21:51:54 +0300

    (Installations) Feat: Latest OpenJDK

--- installations-dnf.sh
+++ installations-dnf.sh
@@ -5,3 +5,5 @@ dnf install -y vim ufw screen \
 	xclip xorg-x11-xauth xorg-x11-utils \
 	xorg-x11-fonts-* xorg-x11-server-Xorg \
 	util-linux-user git zsh;
+
+dnf install -y java-latest-openjdk.x86_64;
