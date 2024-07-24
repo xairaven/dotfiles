96926f56d18eaf5622bf6d3d6810bd047c709681
xairaven <alexkovalevkpi@gmail.com>
2024-07-24 21:29:19 +0300

    (Installations) Feat: util-linux-user (chsh), git, zsh

--- installations-dnf.sh
+++ installations-dnf.sh
@@ -3,4 +3,5 @@
 dnf install -y epel-release;
 dnf install -y vim ufw screen \
 	xclip xorg-x11-xauth xorg-x11-utils \
-	xorg-x11-fonts-* xorg-x11-server-Xorg;
+	xorg-x11-fonts-* xorg-x11-server-Xorg \
+	util-linux-user git zsh;
