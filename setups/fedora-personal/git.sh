953fc607a13f7ddf7b30397926ac09ab2b9e4fbd
xairaven <alexkovalevkpi@gmail.com>
2025-09-04 22:55:29 +0300

    Feat: ooops

--- setups/fedora-personal/git.sh
+++ setups/fedora-personal/git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git config --global user.name "xairaven"
-git config --global user.email mail@gmail.com
+git config --global user.email alexkovalevkpi@gmail.com
 git config --global core.editor vim
 git config --global init.defaultBranch main
