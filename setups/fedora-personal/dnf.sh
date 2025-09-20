f5960199a92906f7276280fad23200d627f49da8
xairaven <alexkovalevkpi@gmail.com>
2025-09-21 00:50:41 +0300

    Feat: DNF + Flatpack

---
+++ setups/fedora-personal/dnf.sh
@@ -0,0 +1,40 @@
+# User
+sudo dnf install \
+    code \
+    fastfetch \
+    git \
+    gzip \
+    htop \
+    putty \
+    python3 \
+    python3-pip \
+    screen \
+    smartmontools \
+    steam \
+    vim \
+    zsh
+
+# System
+sudo dnf install \
+    alsa-sof-firmware \
+    alsa-utils \
+    akmods \
+    bluez \
+    cabextract \
+    cmake \
+    curl \
+    fontconfig \
+    fontconfig-devel \
+    freetype-devel \
+    g++ \
+    kmodtool \
+    libavcodec-freeworld \
+    libxcb-devel \
+    libxkbcommon-devel \
+    linux-firmware \
+    mokutil \
+    mscore-fonts-all \
+    openh264 \
+    openssl \
+    pavucontrol \
+    xorg-x11-font-utils
---
+++ setups/fedora-personal/flatpak.sh
@@ -0,0 +1,22 @@
+# Repositories
+flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/
+
+# Apps
+flatpak install --user flathub \
+    com.discordapp.Discord \
+    com.mojang.Minecraft \
+    com.obsproject.Studio \
+    com.protonvpn.www \
+    com.spotify.Client \
+    com.termius.Termius \
+    com.viber.Viber \
+    md.obsidian.Obsidian \
+    org.chromium.Chromium \
+    org.gimp.GIMP \
+    org.keepassxc.KeePassXC \
+    org.telegram.desktop \
+    org.texstudio.TeXstudio \
+    org.videolan.VLC \
+    org.wireshark.Wireshark \
+    us.zoom.Zoom 
