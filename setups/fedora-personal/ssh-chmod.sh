4fb159522742dc882505afc497778683511da8b0
xairaven <alexkovalevkpi@gmail.com>
2025-11-01 15:28:21 +0200

    (Setups) Feat: Documented ssh-chmod script

--- setups/fedora-personal/ssh-chmod.sh
+++ setups/fedora-personal/ssh-chmod.sh
@@ -1,3 +1,9 @@
+# Protect private keys
 sudo chmod -R 600 /home/xairaven/.ssh
-sudo chmod 644 /home/xairaven/.ssh/github/key.pub                                                  130 ↵
+
+# Public keys can be more permissive
+sudo chmod 644 /home/xairaven/.ssh/github/key.pub
+
+# Ensure directories have the correct permissions
 sudo chmod 700 /home/xairaven/.ssh/
+sudo chmod 700 /home/xairaven/.ssh/github
