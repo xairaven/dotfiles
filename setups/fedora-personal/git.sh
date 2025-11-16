f9347d639bd884d200fafedd0e73dedd9bb6e96a
xairaven <alexkovalevkpi@gmail.com>
2025-11-16 21:41:57 +0200

    (Scripts) Feat: Deleted redundant git setup script

--- setups/fedora-personal/git.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git config --global user.name "xairaven"
-git config --global user.email alexkovalevkpi@gmail.com
-git config --global core.editor vim
-git config --global init.defaultBranch main
