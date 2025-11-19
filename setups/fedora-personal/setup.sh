73f645a5be9bc16b0847e9a3b16ba546c4c484df
xairaven <alexkovalevkpi@gmail.com>
2025-11-19 10:48:53 +0200

    (Fedora) Feat: DNF Install Limit

--- setups/fedora-personal/setup.sh
+++ setups/fedora-personal/setup.sh
@@ -8,3 +8,11 @@ timedatectl set-timezone Europe/Kyiv
 
 # Changing hostname
 hostnamectl set-hostname desktop
+
+# DNF Install Only limit
+# Check if the setting exists; if yes, replace it. If no, append it.
+if grep -q "^installonly_limit=" /etc/dnf/dnf.conf; then
+    sudo sed -i 's/^installonly_limit=.*/installonly_limit=2/' /etc/dnf/dnf.conf
+else
+    echo "installonly_limit=2" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
+fi
